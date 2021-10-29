ce6680df380ddfcc10ad5a6043d2557975992999
Ben Prothe <beprothe@gmail.com>
2021-10-29 16:15:39 -0400

    configured mockAPI submittal

--- front-end/src/temp_components/NewUserForm.tsx
+++ front-end/src/temp_components/NewUserForm.tsx
@@ -1,10 +1,27 @@
 import { useState } from 'react'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:5000'
 
 const NewUserForm = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleSubmit = () => {}
+  const handleSubmit = async () => {
+    const now = Date.now
+    const data = {
+      username: username,
+      password: password,
+      createdAt: now,
+      watching: []
+    }
+    try {
+      const response = await axios.post('/users', data)
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <form onSubmit={handleSubmit}>
